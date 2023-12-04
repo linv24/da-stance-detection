371fb1a727f9415bfd1636ce089ec412862a34ef
Vincent Lin <linv@mit.edu>
2023-12-04 15:01:49 -0500

    add semeval data util

---
+++ data/data_util.py
@@ -0,0 +1,92 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader, Dataset
+import pandas as pd
+from transformers import AutoTokenizer
+
+class SemEval2016Dataset(Dataset):
+    def __init__(self, csv_file, tokenizer, max_length):
+        stance2id = {
+            'AGAINST': 0,
+            'FAVOR': 1,
+            'NONE': 2
+        }
+        target2id = {
+            'Atheism': 0,
+            'Climate Change is a Real Concern': 1,
+            'Donald Trump': 2,
+            'Feminist Movement': 3,
+            'Hillary Clinton': 4,
+            'Legalization of Abortion': 5
+        }
+        col_names = ['tweet', 'target', 'stance', 'opinion towards', 'sentiment']
+
+        df = pd.read_csv(csv_file, encoding='unicode_escape', lineterminator='\r',
+                         header=0, names = col_names)
+        df['stance'] = df['stance'].apply(lambda x: stance2id[x])
+        df['target'] = df['target'].apply(lambda x: target2id[x])
+        self.df = df
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __len__(self):
+        return self.df.shape[0]
+
+    def __getitem__(self, ix):
+        tweet = self.df['tweet'].iloc[ix]
+        target = self.df['target'].iloc[ix]
+        stance = self.df['stance'].iloc[ix]
+        encoding = self.tokenizer(tweet,
+                                  return_tensors='pt',
+                                  max_length=self.max_length,
+                                  padding='max_length',
+                                  truncation=True)
+        input_ids = encoding['input_ids'].flatten()
+        attention_mask = encoding['attention_mask'].flatten()
+        return input_ids, attention_mask, target, stance
+
+def get_semeval_dataset(tokenizer='vinai/bertweet-large', max_length=128, train=True):
+    '''
+    Returns a pandas Dataframe containing semeval data.
+    Header: [tweet, target, stance, opinion towards, sentiment]
+
+    Args:
+        tokenizer: string for hugginface tokenizer
+        max_length: max token length
+        train: bool for accessing either train or test data
+    '''
+    if train:
+        csv_file = './semeval-2016/train.csv'
+    else:
+        csv_file = './semeval-2016/test.csv'
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer)
+
+    return SemEval2016Dataset(csv_file, tokenizer, max_length)
+
+def get_semeval_data_loader(batch_size=128, shuffle=True,
+                            tokenizer='vinai/bertweet-large', max_length=128,
+                            train=True):
+    '''
+    Returns a PyTorch DataLoader for serving semeval data. See below/jupyter
+    notebook for example usage.
+
+    Args:
+        train: bool for accessing either train or test data
+    '''
+    data = get_semeval_dataset(tokenizer, max_length, train)
+    return DataLoader(data, batch_size, shuffle)
+
+
+
+if __name__ == '__main__':
+    dataset = get_semeval_dataset()
+    print(dataset[0])
+
+    loader = get_semeval_data_loader(batch_size=4)
+for ix, (input_ids, attention_mask, target, stance) in enumerate(loader):
+    # print(f'{input_ids=}')
+    # print(f'{attention_mask=}')
+    print(f'{target=}')
+    print(f'{stance=}')
+    if ix == 5:
+        break
